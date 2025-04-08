682d09e1e8b39c10a25e1e9d817db1c03fd0c36c
marianekarolines <nanikaroline292@gmail.com>
2025-04-08 00:50:42 -0300

    configurações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutHeaderComponent } from './components/layout/header/header.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [
+        RouterOutlet,
+
+        LayoutHeaderComponent
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'instituto-tecnico-sp-app';
+    title = 'instituto-tecnico-sp-app';
 }
