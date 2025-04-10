23b32c2f248215c73f7788d0e074be7640aa7e65
marianekarolines <nanikaroline292@gmail.com>
2025-04-10 12:18:18 -0300

    seção siga-nos

---
+++ src/app/components/home/siga-nos/siga-nos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-home-siga-nos',
+    templateUrl: './siga-nos.component.html',
+    styleUrl: './siga-nos.component.scss'
+})
+export class HomeSigaNosComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
